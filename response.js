f799e9a346439a775cc323908a2e8a5f0893e723
Nayan <dreamerboynayan@gmail.com>
2025-04-11 22:25:36 +0600

    add simple string response method

--- response.js
+++ response.js
@@ -7,6 +7,32 @@ app.get("/home" ,(req, res) => {
     res.send("Hello This is MY SERVER");
 });
 
+
+// Simple String Response 
+
+app.get("/one", (req, res) => {
+    res.send("Hello this is simple string response get method.")
+})
+
+app.post("/two", (req, res) => {
+    res.send("Hello this is simple string response post method.")
+})
+
+
+app.put("/three", (req, res) => {
+    res.send("Hello this is simple string response put method")
+})
+
+
+app.delete("/four", (req, res) => {
+    res.send("Hello this is simple string response delete method")
+})
+
+
+
+
+
+
 app.listen(port, () => {
     console.log("My Server is running successfully.")
 })
